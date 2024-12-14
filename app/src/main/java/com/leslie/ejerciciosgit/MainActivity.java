925505be89e25a362845f0da67581812c5b2cf27
Danaet96 <masl960804@gmail.com>
2024-12-13 21:21:05 -0600

    Cambie de sumar a Saludar

--- app/src/main/java/com/leslie/ejerciciosgit/MainActivity.java
+++ app/src/main/java/com/leslie/ejerciciosgit/MainActivity.java
@@ -2,15 +2,35 @@ package com.leslie.ejerciciosgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
+   public EditText nombre,apellido;
+   public TextView ress;
+   public Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // :) :0
+         nombre=findViewById(R.id.tv1);
+         apellido=findViewById(R.id.tv2);
+         ress=findViewById(R.id.tvres);
+         boton=findViewById(R.id.btnSaludar);
+
+
+
+        boton.setOnClickListener(v->{
+
+            ress.setText("Hola "+nombre.getText()+" "+apellido.getText()+" Es un gusto!");
+
+        });
+
     }
 }
+
